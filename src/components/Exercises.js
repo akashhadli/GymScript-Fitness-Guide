1de2a6966beee4029b0e77e1e61f50b93a49bb98
Akash Hadli <aakashhadli@gmail.com>
2023-03-24 19:59:50 +0530

    display searched exercises
    created the exercise card component to display each searched exercise in a card.
    also added the Pagination feature to display only 9 exercises in one page.

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Pagination from '@mui/material/Pagination';
+import { Box, Stack, Typography } from '@mui/material';
 
-const Exercises = () => {
-	return <div>Exercises</div>;
+import ExerciseCard from './ExerciseCard';
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+	const [currentPage, setcurrentPage] = useState(1);
+	const exercisesPerPage = 9;
+	const indexOfLastExercise = currentPage * exercisesPerPage;
+	const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+	const currentExercises = exercises.slice(
+		indexOfFirstExercise,
+		indexOfLastExercise
+	);
+	const paginate = (e, value) => {
+		setcurrentPage(value);
+
+		window.scrollTo({ top: 1800, behavior: 'smooth' });
+	};
+
+	return (
+		<Box id='exercises' sx={{ mt: { lg: '110px' } }} mt='50px' p='20px'>
+			<Typography variant='h3' mb='46px'>
+				Showing Exercises
+			</Typography>
+			<Stack
+				direction='row'
+				sx={{ gap: { lg: '110px', xs: '50px' } }}
+				flexWrap='wrap'
+				justifyContent='center'
+			>
+				{currentExercises.map((exercise, index) => (
+					<ExerciseCard key={index} exercise={exercise} />
+				))}
+			</Stack>
+			<Stack sx={{ mt: { lg: '114px', xs: '70px' } }} alignItems='center'>
+				{exercises.length > 9 && (
+					<Pagination
+						color='standard'
+						shape='rounded'
+						defaultPage={1}
+						count={Math.ceil(exercises.length / exercisesPerPage)}
+						page={currentPage}
+						onChange={paginate}
+						size='large'
+					/>
+				)}
+			</Stack>
+		</Box>
+	);
 };
 
 export default Exercises;
