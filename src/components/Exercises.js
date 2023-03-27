2caf37675f50ba53b5b400ddc48c0692bf943726
Akash Hadli <aakashhadli@gmail.com>
2023-03-27 19:18:50 +0530

    Display selected category exercises from category search filter

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
@@ -18,6 +19,28 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
 		window.scrollTo({ top: 1800, behavior: 'smooth' });
 	};
+
+	useEffect(() => {
+		const fetchExercisesData = async () => {
+			let exercisesData = [];
+
+			if (bodyPart === 'all') {
+				exercisesData = await fetchData(
+					'https://exercisedb.p.rapidapi.com/exercises',
+					exerciseOptions
+				);
+			} else {
+				exercisesData = await fetchData(
+					`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+					exerciseOptions
+				);
+			}
+
+			setExercises(exercisesData);
+		};
+
+		fetchExercisesData();
+	}, [bodyPart]);
 
 	return (
 		<Box id='exercises' sx={{ mt: { lg: '110px' } }} mt='50px' p='20px'>
